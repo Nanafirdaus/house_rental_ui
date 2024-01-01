df158ae0d81b78ec8970613641744e17b40349a4
firdaus <nanafirdaus02@gmail.com>
2024-01-01 16:07:08 +0100

    Finished the basic layout of the onbaording page

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:house_rental_ui/on_boarding.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: OnBoardingScreen(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
